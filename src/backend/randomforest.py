2ce01953fefe1e5aa6a43cfaf7e2d945ed80ec67
OEMarshall <oemarsha@uncg.edu>
2017-04-15 16:45:45 -0400

    Filtered to only 11 features

--- src/backend/randomforest.py
+++ src/backend/randomforest.py
@@ -8,6 +8,7 @@ from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.externals import joblib
+from sklearn.model_selection import KFold
 
 f = open(sys.argv[1],'rb')
 reader = csv.reader(f)
@@ -19,7 +20,16 @@ df = df.apply(le.fit_transform) # changes str val in features to ints
 #data split
 np.random.shuffle(df.values)
 all_val = df.drop('sold_binary', axis=1)
+all_val = all_val.drop('year_updated', axis=1)
+all_val = all_val.drop('lot_size_sq_footage', axis=1)
+all_val = all_val.drop('street_address', axis=1)
+all_val = all_val.drop('city',axis=1)
+all_val = all_val.drop('state',axis=1)
+all_val = all_val.drop('zip',axis=1)
+all_val = all_val.drop('roof_type',axis=1)
 all_label = df[['sold_binary']]
+
+print all_val
 
 observations = len(all_val.index)
 trainTestSplit = observations // 3
@@ -29,9 +39,16 @@ train_label = all_label.head(observations - trainTestSplit)
 test_val = all_val.tail(trainTestSplit)
 test_label = all_label.tail(trainTestSplit)
 
-rf = RandomForestClassifier(n_estimators=2000,oob_score=True)
+kf = KFold(n_splits=10, random_state=0)
+kf.get_n_splits(all_val)
+print kf
+
+rf = RandomForestClassifier(n_estimators=3500,oob_score=True,n_jobs=-1)
+
 rf.fit(train_val.values,train_label.values.ravel())
 predicted = rf.predict(test_val.values)
+
+
 
 print "Random Forest accuracy score is: " + str(accuracy_score(test_label.values,predicted,normalize='False'))
 print str("Out of Bag score is: " + str(rf.oob_score_))
