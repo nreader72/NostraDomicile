d5082e0238e7de7e369441d86460ad3120f1ff1f
Ctsimaan <ctsimaan@gmail.com>
2017-04-19 20:25:41 -0400

    Update attribute_classifier (1).py

--- src/backend/RandomForests/attribute_classifier (1).py
+++ src/backend/RandomForests/attribute_classifier (1).py
@@ -15,79 +15,80 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_recall_fscore_support
 import matplotlib.pyplot as plt
 
-def attribute_classifier(zip_code)
+def attribute_classifier(zip_code):
     cnx = mysql.connector.connect(user='ctsimaan',password='SeniorProject490',
-                              host='nostradomicile-data.c6x7vypetdqh.us-west-2.rds.amazonaws.com',
-                              database='PyZillow_Data')
+        host='nostradomicile-data.c6x7vypetdqh.us-west-2.rds.amazonaws.com',
+        database='PyZillow_Data')
     cursor = cnx.cursor()
 
 
-# In[2]:
+    # In[2]:
 
-df_pd = pd.read_sql('SELECT*FROM PyZillow_Data.home_data Where zip = 27705', con=cnx)
-#print(df_pd)
-frames = [df_pd,df_pd]
-df_pd = pd.concat(frames)
-#list(df_pd)
+    df_pd = pd.read_sql('SELECT*FROM PyZillow_Data.home_data Where zip = 27705', con = cnx)
+    #print(df_pd)
+    frames = [df_pd,df_pd]
+    df_pd = pd.concat(frames)
+    #list(df_pd)
 
 
-# In[3]:
+    # In[3]:
 
-#new = df_pd.filter(['sold_binary', 'home_type', 'bedrooms', 'bathrooms', 'finished_sq_footage',
-       #'lot_size_sq_footage','year_built','last_sale_price','neighborhood', 'school_district'], axis = 1)
+    #new = df_pd.filter(['sold_binary', 'home_type', 'bedrooms', 'bathrooms', 'finished_sq_footage',
+    #'lot_size_sq_footage','year_built','last_sale_price','neighborhood', 'school_district'], axis = 1)
     df_pd.drop(['street_address','zip','city', 'state','room_types','roof_type','last_sold_date','heating_system'],inplace=True,axis=1)
     cols_to_transform = [ 'home_type','neighborhood','school_district','appliances','floor_covering','heating_sources','parking_type']
     df_dum = pd.get_dummies(data=df_pd,columns = cols_to_transform)
     df_dum = df_dum.fillna(df_dum.mean())
 
 
-# In[4]:
+    # In[4]:
 
-train, test = train_test_split(df_dum, test_size = 0.2)
-#how to fix sold binary showing up in wrong position
-bSoldDF = pd.DataFrame(df_dum.pop('sold_binary'))
-#dfs= [bSoldDF,df_dum]
-df_dum = pd.concat([bSoldDF,df_dum],axis=1)
-col_length = len(df_dum.columns)
-#have to get the length of df_dum before this
-features = df_dum.columns[1:col_length]
-#df_dum =df_dum.reindex(columns=['sold_binary'],cols[-1:].union(cols[:-1]) )
-#df_dum.insert('bedrooms', 'sold_binary', df_dum.mean(1))
-#print(features)
+    train, test = train_test_split(df_dum, test_size = 0.2)
+    #how to fix sold binary showing up in wrong position
+    bSoldDF = pd.DataFrame(df_dum.pop('sold_binary'))
+    #dfs= [bSoldDF,df_dum]
+    df_dum = pd.concat([bSoldDF,df_dum],axis=1)
+    col_length = len(df_dum.columns)
+    #have to get the length of df_dum before this
+    features = df_dum.columns[1:col_length]
+    #df_dum =df_dum.reindex(columns=['sold_binary'],cols[-1:].union(cols[:-1]) )
+    #df_dum.insert('bedrooms', 'sold_binary', df_dum.mean(1))
+    #print(features)
 
 
-# In[5]:
+    # In[5]:
 
-seed = 7
-max_features = 3
-#rf = RandomForestClassifier(n_estimators=100, max_features="auto",oob_score = True, 
+    seed = 7
+    max_features = 3
+    #rf = RandomForestClassifier(n_estimators=100, max_features="auto",oob_score = True, 
                         #n_jobs = -1,random_state =50,min_samples_leaf = 50)
-rf = RandomForestClassifier(n_estimators=100, max_features=max_features)
-y, _ = pd.factorize(train['sold_binary'])
-#y = train['sold_binary']
-rf.fit(train[features], y)
-#rf.fit(df_dum,y)
-preds = rf.predict(test[features])
-#preds = rf.predict(df_dum)
-kfold = model_selection.KFold(n_splits=10, random_state=seed)
-results = model_selection.cross_val_score(rf, test[features], test['sold_binary'], cv=kfold)
-#print(precision_recall_fscore_support(test['sold_binary'], preds, average='micro'))
-#print(results.mean())
-feature_importance = rf.feature_importances_
-#print "Features sorted by their score:"
-#print sorted(zip(map(lambda x: round(x, 4), rf.feature_importances_), features), 
-         #reverse=True)
-#print(rf.feature_importances_)
+    rf = RandomForestClassifier(n_estimators=100, max_features=max_features)
+    y, _ = pd.factorize(train['sold_binary'])
+    #y = train['sold_binary']
+    rf.fit(train[features], y)
+    #rf.fit(df_dum,y)
+    preds = rf.predict(test[features])
+    #preds = rf.predict(df_dum)
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    results = model_selection.cross_val_score(rf, test[features], test['sold_binary'], cv=kfold)
+    #print(precision_recall_fscore_support(test['sold_binary'], preds, average='micro'))
+    #print(results.mean())
+    feature_importance = rf.feature_importances_
+    #print "Features sorted by their score:"
+    print sorted(zip(map(lambda x: round(x, 4), rf.feature_importances_), features))     
+	#reverse=True)
+    #print(rf.feature_importances_)
 
 
-# In[6]:
+    # In[6]:
 
-# make importances relative to max importance
+    # make importances relative to max importance
     feature_importance = 100.0 * (feature_importance / feature_importance.max())
  
-# A threshold below which to drop features from the final data set. Specifically, this number represents
-# the percentage of the most important feature's importance value
-    fi_threshold = 15
+    # A threshold below which to drop features from the final data set. Specifically, this number represents
+    # the percentage of the most important feature's importance value
+    # @ctsimaan - increased fi_threshold to 50 from 15 in order to reduce list of output attributes.
+    fi_threshold = 50
  
 # Get the indexes of all features over the importance threshold
     important_idx = np.where(feature_importance > fi_threshold)[0]
@@ -121,6 +122,5 @@ feature_importance = rf.feature_importances_
 
 
 # In[ ]:
-
 
 
