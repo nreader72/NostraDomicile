6e10f506ade8df6016d5547c9209dd1d62e74575
Jeremy Hutton <jlhutton@uncg.edu>
2017-04-24 00:17:48 -0400

    functions for chart making

---
+++ src/backend/RandomForests/Chart_Work.py
@@ -0,0 +1,58 @@
+
+# coding: utf-8
+
+# In[5]:
+
+import mysql
+import mysql.connector
+import pandas as pd
+import numpy as np
+import plotly.tools as tls
+#tls.embed('https://plot.ly/~cufflinks/8')
+
+
+# In[6]:
+
+def dfMaker(user,password,host,database,zipCode):
+    cnx = mysql.connector.connect(user=user,password=password,
+                                  host=host,database=database)
+    cursor = cnx.cursor()
+    df = pd.read_sql('SELECT*FROM PyZillow_Data.home_data Where zip =' + zipCode, con=cnx)
+    return df
+
+def housePriceMean(dataFrame):
+    mean = dataFrame['last_sale_price'].mean()
+    return mean
+
+def sqFootMean(dataFrame):
+    mean = dataFrame['finished_sq_footage'].mean()
+    return mean
+
+def homes_bedrooms_df(dataframe):
+    df = dataframe.filter(items=['bedrooms'])
+    return df
+
+def soldOrNot_addr(dataframe):
+    df = dataframe.filter(items=['street_address','sold_binary'])
+    return df
+
+def main():
+    df = dfMaker('ctsimaan','SeniorProject490','nostradomicile-data.c6x7vypetdqh.us-west-2.rds.amazonaws.com',
+           'PyZillow_Data','27705')
+    #print(df)
+    #print(housePriceMean(df))
+    #print(homes_bedrooms_df(df))
+    print(soldOrNot_addr(df))
+    
+    
+
+
+# In[7]:
+
+main()
+
+
+# In[ ]:
+
+
+
