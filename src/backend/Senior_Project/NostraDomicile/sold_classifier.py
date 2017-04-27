2dafe2f6a49b29b691041d535822d49d9c226f48
Jeremy Hutton <jlhutton@uncg.edu>
2017-04-26 21:02:26 -0400

    Update sold_classifier.py

--- src/backend/Senior_Project/NostraDomicile/sold_classifier.py
+++ src/backend/Senior_Project/NostraDomicile/sold_classifier.py
@@ -18,9 +18,9 @@ from sklearn.preprocessing import Imputer
 
 
 #zip code for this is 27705
-def sold_classifier(zip_code):
-  #def sold_classifier(zip_code,bedrooms,bathrooms,finished_sq_footage,lot_size_sq_footage,year_built,last_sale_price,home_type,
-                   #neighborhood,school_district,parking_type,number_of_floors):
+#def sold_classifier(zip_code):
+  def sold_classifier(zip_code,bedrooms,bathrooms,finished_sq_footage,lot_size_sq_footage,year_built,last_sale_price,home_type,
+                   neighborhood,school_district,parking_type,number_of_floors):
     cnx = mysql.connector.connect(user='ctsimaan',password='SeniorProject490',
                               host='nostradomicile-data.c6x7vypetdqh.us-west-2.rds.amazonaws.com',
                               database='PyZillow_Data')
@@ -50,10 +50,10 @@ def sold_classifier(zip_code):
     
     array = df_dum.values
     train, test = train_test_split(df_dum, test_size = 0.3)
-    #d={}
-    #d = {'sold_binary':0, 'bedrooms':bedrooms,'bathrooms':bathrooms,'finished_sq_footage':finished_sq_footage,
-         #'lot_size_sq_footage':lot_size_sq_footage,'year_built':year_built, 'last_sale_price':last_sale_price,'home_type':home_type,'neighborhood':
-         #neighborhood, 'school_district':school_district, 'parking_type':parking_type,'number_of_floors':number_of_floors}
+    d={}
+    d = {'sold_binary':0, 'bedrooms':bedrooms,'bathrooms':bathrooms,'finished_sq_footage':finished_sq_footage,
+         'lot_size_sq_footage':lot_size_sq_footage,'year_built':year_built, 'last_sale_price':last_sale_price,'home_type':home_type,'neighborhood':
+         neighborhood, 'school_district':school_district, 'parking_type':parking_type,'number_of_floors':number_of_floors}
     rowCount = len(df_dum.index)
     #d = pd.DataFrame(data = d, index=[rowCount])
     #cols_to_transform = [ 'home_type','neighborhood','school_district','parking_type']
